cefb9279c2b537b762cc1b91e5a4d182042f4257
Jean-Philippe Allard <jp@jpallard.com>
2023-10-03 21:49:18 -0400

    First working version (?)

--- postMessage_receiver.js
+++ postMessage_receiver.js
@@ -3,10 +3,8 @@ const allowedOrigins = ['https://form.jotform.com', 'https://widgets.jotform.io'
 window.addEventListener('message', (event) => {
     // Check if the origin is one of the allowed origins
     if (allowedOrigins.includes(event.origin)) {
-        console.log('Received message:', event);
-        debugger
         // Check if the event name is the one we want
-        if (event.data.event_name == 'form_page_visible') {
+        if (event.data.event_name == 'form_page_visible' || event.data.event_name == 'form_submitted') {
             window.dataLayer = window.dataLayer || [];
             window.dataLayer.push({
                 event: event.data.event_name,
@@ -14,22 +12,5 @@ window.addEventListener('message', (event) => {
                 page: event.data.page
             });
         }
-        if (event.data.type == 'submit') {
-            console.log('Submit event: ', event)
-            debugger
-        }
     }
 }, false);
-
-// Listening to all submit events
-document.addEventListener('submit', function(event) {
-    // Get the form element from the event's target
-    console.log('Event is: ', event)
-    debugger
-    const form = event.target;
-    // Check if the form's action attribute starts with the desired URL prefix
-    if (form.action && form.action.startsWith("https://submit.jotform.com/submit/")) {
-        // Your custom event handling logic here
-        console.log('Form submitted:', form);
-    }
-});
