0a347221c4a4f31a7cc3eabeff8d455cc55b0a6d
Jean-Philippe Allard <jp@jpallard.com>
2023-10-04 16:54:30 -0400

    Pushing check for double data in the top level context

--- postMessage_receiver.js
+++ postMessage_receiver.js
@@ -1,16 +1,30 @@
+// Function to test for object existence
+function objectExists(arr, obj) {
+    return arr.some(item => {
+        return Object.keys(obj).every(key => obj[key] === item[key]) && 
+               Object.keys(item).length === Object.keys(obj).length;
+    });
+}
+
 // Listening to widget page messages
 const allowedOrigins = ['https://form.jotform.com', 'https://widgets.jotform.io', 'https://jotform-widget.pages.dev'];
+let pushedEvents = []; // Accumulating pushed events to make sure we don't send one twice. 
 window.addEventListener('message', (event) => {
     // Check if the origin is one of the allowed origins
     if (allowedOrigins.includes(event.origin)) {
         // Check if the event name is the one we want
         if (event.data.event_name == 'form_page_visible' || event.data.event_name == 'form_submitted') {
-            window.dataLayer = window.dataLayer || [];
-            window.dataLayer.push({
+            const pushedData = {
                 event: event.data.event_name,
                 formName: event.data.formName,
                 page: event.data.page
-            });
+            }
+            // Make sure we haven't pushed this event before
+            if (!objectExists(pushedEvents, pushedData)) {
+                window.dataLayer = window.dataLayer || [];
+                window.dataLayer.push(pushedData);
+                pushedEvents.push(pushedData)
+            }
         }
     }
 }, false);
